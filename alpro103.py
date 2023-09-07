2c2506544687337ba03dc57b91f5f311fa482929
Rendy Putra Pratama <110386715+reendyp@users.noreply.github.com>
2023-09-07 07:22:36 +0700

    Create alpro103.py

---
+++ alpro103.py
@@ -0,0 +1,17 @@
+# Percobaan 3: Tipe Data (a) bilangan
+# alpro103.py
+m = 3.5
+n = 2
+
+print("Nilai m = %f" % m)
+print("Nilai n = %d" % n) # apa fungsi %f dan %d ?
+# kode diatas menjelaskan nilai bilangan desimal yang kemudian di ubah oleh fungsi %f dan %d
+
+#ubah %f dan %d
+print("Nilai m = %d" % m)
+print("Nilai n = %f" % n)
+# %d berfungsi untuk membulatkan bilangan desimal
+# %f untuk membuat bilangan real 
+x = m*n
+print(" m x n = %f" % x) # hasil = 7.00000 (real floating point)
+print(" m x n = %d" % x) # hasil = 7. bilangan bulat desimal
