e179fc2b5f52a876e8454146f4cf44de5835cba3
Rendy Putra Pratama <110386715+reendyp@users.noreply.github.com>
2023-09-07 07:23:05 +0700

    Create alpro104.py

---
+++ alpro104.py
@@ -0,0 +1,13 @@
+#percobaan 4 tipe data (b) karakter
+a = 'Algoritma'
+b = "Pemrograman"
+c = 'Bahasa'
+d = "Python"
+
+print("Nilai a = %s" % a)
+print("Nilai b = %r" % b)
+print("Nilai c = %r" % c)
+print("Nilai d = %r" % d)
+
+# fungsi %s untuk mengkonversi fungsi str (). dapat dilihat pada baris 7 dan 8. jika menggunakan print("hello") harus menggunakan kutip, namun pada hal ini
+# fungsi %r untuk memunculkan string ''
