f1dc5e2ea0ef075f68f3b1dc9372c5fb02efde49
Rendy Putra Pratama <110386715+reendyp@users.noreply.github.com>
2023-09-07 07:18:53 +0700

    alpro101.py

---
+++ alpro101.py
@@ -0,0 +1,7 @@
+# Percobaan 1. Halo dunia 
+#alpro101.py
+print("Rendy Putra Pratama")
+# print merupakan perintah untuk memberikan output dari 
+print(10)
+
+#dapat dilihat untuk mencetak angka 10 tidak dibutuhkan string (kutip dua atau satu). Sedangkan, karakter membutuhkan string seperti print("Rendy Putra Pratama")
