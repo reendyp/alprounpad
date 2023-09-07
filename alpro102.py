38b60ffe000de56df8b98d77b8212c4c0bcebd7e
Rendy Putra Pratama <110386715+reendyp@users.noreply.github.com>
2023-09-07 07:21:59 +0700

    Create alpro102.py

---
+++ alpro102.py
@@ -0,0 +1,16 @@
+# Percobaan 2: Penggunaan Variabel
+# alpro102.py
+n = 140310230037
+# n sebagai variabel, dan npm sebagai value 
+
+print("Nilai n = ", n) #n berfungsi untuk memanggil variabel yang telah dideklarasikan sebagai nomor npm pada baris 3.
+
+#NEXT. coba ubah npm dengan nama. akan ditemukan kesalahan. Cara memperbaikinya dengan menggunakan string "" atau '' pada value yang ingin ditambahkan
+
+m = "Rendy Putra Pratama" # string dapat dibuat menggunakan tanda kutip dua "" atau kutip 1 ''. Fungsi string ialah untuk mendeklarasikan value yang bersifat karakter dalam hal ini huruf.
+print("Nama : ", m)
+
+#dapat dilihat untuk mencetak angka npm tidak dibutuhkan string (kutip dua atau satu). Sedangkan, karakter membutuhkan string seperti print("Rendy Putra Pratama")
+
+# Hal yang telah dipelajari :
+# Mengetahui fungsi string ("" atau ' ') untuk memberikan value karakter (huruf dsb) pada variabel agar terdeklarasi
